c8ecd65c447c1785cb9240e2d04459f8a2e302ce
alok98 <46619091+alok98@users.noreply.github.com>
2019-08-14 15:56:16 +0530

    setup of static files for pages

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app=express();
 const port=8000;
 const expressLayouts=require("express-ejs-layouts");
 app.use(expressLayouts);
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+app.use(express.static('./assets'));
+
+
 //use express router
 app.use('/',require("./routes"));
+
 
 
 // set up the view engine
