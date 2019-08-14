0f5b920cf2b3d9d923f0543a16860636edb62846
alok98 <46619091+alok98@users.noreply.github.com>
2019-08-14 11:37:55 +0530

    add a controller and router for home

---
+++ controllers/homeController.js
@@ -0,0 +1,3 @@
+module.exports.home=function(rep,res){
+    return res.end("<h1>Express is working</h1>");
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const routes=express.Router();
-
+const homeController=require('../controllers/homeController');
+routes.get('/',homeController.home);
 console.log('router loaded');
 module.exports =  routes;
