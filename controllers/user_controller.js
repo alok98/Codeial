520de8f19f89caea6abe9ad7a0a2e70a5f8094de
alok98 <46619091+alok98@users.noreply.github.com>
2019-08-17 17:43:44 +0530

    setup the user schema and render the singup and signin page

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,3 +3,13 @@ module.exports.profile=function(req,res){
        title: "profile"
    });
 }
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Sign Up"
+    })
+}
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"Sign In"
+    })
+}
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/user_controller');
 const post_controller =require('../controllers/post_controller');
 router.get('/profile',userController.profile);
 router.get('/post',post_controller.post);
+router.get('/signIn',userController.signIn);
+router.get('/signUp',userController.signUp);
 
 module.exports=router;
